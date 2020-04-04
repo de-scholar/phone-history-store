b319a84529498d11cc77960bcd014c5f100f3a2f
descolar-ceo <emmanuellamugi@gmail.com>
2020-04-04 12:23:38 +0200

    chore(project):Setup the whole project
    -Initialize the project
    
    [Finishes #172160087]

---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const WelcomeScreen = () => {
+    return (
+        <View>
+        <Text>Welcome to the new world!</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize:40
+    }
+});
