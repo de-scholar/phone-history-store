f35199c618bc941c20799317647b622d0b97fa48
descolar-ceo <emmanuellamugi@gmail.com>
2020-04-04 15:59:20 +0200

    feat(Welcome screen):Setup a welcome screen
    -Add a welcome screen
    -Add routing to WelcomeScreen in App.js file
    -Add icon of phone history store on welcome screen
    
    [Finishes #172160549]

---
+++ assets/text/eng/welcomeText.js
@@ -0,0 +1,1 @@
+export const WELCOME_TEXT = `Welcome to Phone History Store! Here are few steps to follow in order to work with us  perfectly. Okay Let’s go!`
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import IconContainer from '../components/IconContainer';
+import IntroductionTextContainer from '../components/IntroductionTextContainer';
+import { WELCOME_TEXT } from '../../assets/text/eng/welcomeText';
 
 const WelcomeScreen = () => {
     return (
-        <View>
-        <Text>Welcome to the new world!</Text>
+        <View style={styles.container}>
+            <IconContainer iconSource={require('../../assets/img/phone-history-store-icon.png')}/>
+            <IntroductionTextContainer textToShow = {WELCOME_TEXT} />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    text: {
-        fontSize:40
+    container: {
+        backgroundColor: 'rgb(100,120,255)',
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 });
+
+export default WelcomeScreen;
